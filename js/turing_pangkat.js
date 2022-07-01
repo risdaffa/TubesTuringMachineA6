ae110c97e64bb4b41fe56f03f25da7dd459e3ef0
risdaffa <101318571+risdaffa@users.noreply.github.com>
2022-07-01 19:36:08 +0700

    Update turing_pangkat.js

--- js/turing_pangkat.js
+++ js/turing_pangkat.js
@@ -25,21 +25,20 @@ function execute() {
         tmTape.childNodes[1].className += " active";
         it = 2; // Awal head
         state = 0; // Awal state
-
+        
         // Memasukkan 0 sejumlah num1
         for (i = 0; i < num1.value; i++) {
             tapeCells.push(new Cell("0"));
         }
         // Pembatas
-        tapeCells.push(new Cell("1"));
+        tapeCells.push(new Cell("C"));
         // Memasukkan 0 sejumlah num2
         for (i = 0; i < num2.value; i++) {
             tapeCells.push(new Cell("0"));
         }
         // Pembatas
-        tapeCells.push(new Cell("1"));
+        tapeCells.push(new Cell("C"));
         // Blank symbol di akhir tape
-        tapeCells.push(new Cell("B"));
         tapeCells.push(new Cell("B"));
         tapeCells.push(new Cell("B"));
     }
@@ -49,7 +48,7 @@ function execute() {
 function showAns() {
     // Pangkat
     let ans = Math.pow(num1.value, num2.value);
-
+    
     // Menampilkan jawaban
     let ansField = document.getElementById("ans");
     ansField.textContent = ans;
@@ -68,382 +67,366 @@ function executeNextMove() {
 
         // STATE 0
         if (state == 0) {
-            // 0/B,R ke state 1
+            // 0/0,R to state 0
             if (tapeCells[it].symbol == "0") {
-                move("B", 1, 1);
+                move("0", 1, 0);
             }
-            // 1/B,R ke state 23
-            else if (tapeCells[it].symbol == "1") {
-                move("B", 1, 23);
+            // C/C,R to state 1
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 1);
             }
         }
 
         // STATE 1
         else if (state == 1) {
-            // 0/0,R ke state 1
+            // 0/B,L to state 2
             if (tapeCells[it].symbol == "0") {
-                move("0", 1, 1);
-            }
-            // 1/1,R ke state 2
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 2);
+                move("B", -1, 2);
             }
         }
 
         // STATE 2
         else if (state == 2) {
-            // B/B,R ke state 2
-            if (tapeCells[it].symbol == "B") {
-                move("B", 1, 2);
+            // C/C,L to state 2
+            if (tapeCells[it].symbol == "C") {
+                move("C", -1, 2);
             }
-            // 0/B,R ke state 3
+            // 0/X,R to state 3
             else if (tapeCells[it].symbol == "0") {
-                move("B", 1, 3);
-            }
-            // 1/1,R ke state 14
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 14);
+                move("X", 1, 3);
             }
         }
 
         // STATE 3
         else if (state == 3) {
-            // 0/0,R ke state 3
-            if (tapeCells[it].symbol == "0") {
-                move("0", 1, 3);
+            // X/X,R to state 3
+            if (tapeCells[it].symbol == "X") {
+                move("X", 1, 3);
             }
-            // 1/1,R ke state 4
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 4);
+            // C/C,R to state 4
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 4);
             }
         }
 
         // STATE 4
         else if (state == 4) {
-            // 0/0,R ke state 4
+            // 0/0,R to state 4
             if (tapeCells[it].symbol == "0") {
                 move("0", 1, 4);
             }
-            // X/X,R ke state 4
-            else if (tapeCells[it].symbol == "X") {
-                move("X", 1, 4);
-            }
-            // B/0,L ke state 5
+            // B/B,R to state 4
             else if (tapeCells[it].symbol == "B") {
-                move("0", -1, 5, 1);
+                move("B", 1, 4);
             }
-            // 1/1,L ke state 7
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 7);
-            }
+            // C/C,R to state 5
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 5);
+            }   
         }
 
         // STATE 5
         else if (state == 5) {
-            // 0/0,L ke state 5
+            // 0/0,R to state 5
             if (tapeCells[it].symbol == "0") {
-                move("0", -1, 5);
+                move("0", 1, 5);
             }
-            // 1/1,L ke state 6
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 6);
+            // B/0,L to state 6
+            else if (tapeCells[it].symbol == "B") {
+                move("0", -1, 6, 1);
             }
         }
 
         // STATE 6
         else if (state == 6) {
-            // 0/0,L ke state 6
+            // 0/0,L to state 6
             if (tapeCells[it].symbol == "0") {
                 move("0", -1, 6);
             }
-            // B/B,R ke state 2
+            // C/C,L to state 6
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 6);
+            }
+            // B/B,L to state 6
             else if (tapeCells[it].symbol == "B") {
-                move("B", 1, 2);
+                move("B", -1, 6);
+            }
+            // X/X,L to state 7
+            else if (tapeCells[it].symbol == "X") {
+                move("X", -1, 7);
             }
         }
 
         // STATE 7
         else if (state == 7) {
-            // B/B,L ke state 7
-            if (tapeCells[it].symbol == "B") {
-                move("B", -1, 7);
+            // X/X,L to state 7
+            if (tapeCells[it].symbol == "X") {
+                move("X", -1, 7);
             }
-            // 0/B,R ke state 8
+            // 0/X,R to state 3
             else if (tapeCells[it].symbol == "0") {
+                move("X", 1, 3);
+            }
+            // B/B,R to state 8
+            else if (tapeCells[it].symbol == "B") {
                 move("B", 1, 8);
-            }
-            // 1/1,R ke state 11
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 11);
-            }
-            // X/X,R ke state 11
-            else if (tapeCells[it].symbol == "X") {
-                move("X", 1, 11);
             }
         }
 
         // STATE 8
         else if (state == 8) {
-            // B/B,R ke state 8
-            if (tapeCells[it].symbol == "B") {
-                move("B", 1, 8);
+            // C/C,L to state 8
+            if (tapeCells[it].symbol == "C") {
+                move("C", -1, 8);
             }
-            // 1/1,R ke state 9
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 9);
+            // X/0,R to state 8
+            else if (tapeCells[it].symbol == "X") {
+                move("0", 1, 8);
+            }
+            // 0/X,R to state 9
+            else if (tapeCells[it].symbol == "0") {
+                move("X", 1, 9);
             }
         }
 
         // STATE 9
         else if (state == 9) {
-            // 0/0,R ke state 9
-            if (tapeCells[it].symbol == "0") {
-                move("0", 1, 9);
-            }
-            // B/0,L ke state 10
-            else if (tapeCells[it].symbol == "B") {
-                move("0", -1, 10, 1);
+            // C/C,R to state 10
+            if (tapeCells[it].symbol == "C") {
+                move("C", 1, 10);
             }
         }
 
         // STATE 10
         else if (state == 10) {
-            // 0/0,L ke state 10
-            if (tapeCells[it].symbol == "0") {
-                move("0", -1, 10);
+            // B/B,R to state 10
+            if (tapeCells[it].symbol == "B") {
+                move("B", 1, 10);
             }
-            // 1/1,L ke state 7
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 7);
+            // 0/B,L to state 11
+            else if (tapeCells[it].symbol == "0") {
+                move("B", -1, 11);
+            }
+            // C/C,L to state 23
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 23);
             }
         }
 
         // STATE 11
-        else if (state == 1) {
-            // B/0,R ke state 11
-            if (tapeCells[it].symbol == "B") {
-                move("0", 1, 11);
+        else if (state == 11) {
+            // 0/0,L to state 11
+            if (tapeCells[it].symbol == "0") {
+                move("0", -1, 11);
             }
-            // 1/1,L ke state 12
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 12);
+            // B/B,L to state 11
+            else if (tapeCells[it].symbol == "B") {
+                move("B", -1, 11);
+            }
+            // C/C,L to state 12
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 12);
             }
         }
 
         // STATE 12
         else if (state == 12) {
-            // 0/0,L ke state 12
-            if (tapeCells[it].symbol == "0") {
-                move("0", -1, 12);
-            }
-            // X/X,L ke state 12
-            else if (tapeCells[it].symbol == "X") {
+            // X/X,L to state 12
+            if (tapeCells[it].symbol == "X") {
                 move("X", -1, 12);
             }
-            // 1/1,L ke state 13
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 13);
+            // 0/X,R to state 13
+            else if (tapeCells[it].symbol == "0") {
+                move("X", 1, 13);
+            }
+            // B/B,R to state 19
+            else if (tapeCells[it].symbol == "B") {
+                move("B", 1, 19);
             }
         }
 
         // STATE 13
         else if (state == 13) {
-            // 0/0,L ke state 13
-            if (tapeCells[it].symbol == "0") {
-                move("0", -1, 13);
+            // X/X,R to state 13
+            if (tapeCells[it].symbol == "X") {
+                move("X", 1, 13);
             }
-            // B/B,R ke state 2
-            else if (tapeCells[it].symbol == "B") {
-                move("B", 1, 2);
+            // C/C,R to state 14
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 14);
             }
         }
 
         // STATE 14
         else if (state == 14) {
-            // 0/0,R ke state 14
+            // 0/0,R to state 14
             if (tapeCells[it].symbol == "0") {
                 move("0", 1, 14);
             }
-            // X/X,R ke state 14
-            else if (tapeCells[it].symbol == "X") {
-                move("X", 1, 14);
-            }
-            // B/1,L ke state 15
+            // B/B,R to state 14
             else if (tapeCells[it].symbol == "B") {
-                move("1", -1, 15, 1);
+                move("B", 1, 14);
             }
-            // 1/X,L ke state 18
-            else if (tapeCells[it].symbol == "1") {
-                move("X", -1, 18);
+            // C/C,R to state 15
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 15);
             }
         }
 
         // STATE 15
         else if (state == 15) {
-            // 0/0,L ke state 15
+            // 0/0,R to state 15
             if (tapeCells[it].symbol == "0") {
-                move("0", -1, 15);
+                move("0", 1, 15);
             }
-            // 1/1,L ke state 16
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 16);
+            // Y/Y,R to state 15
+            else if (tapeCells[it].symbol == "Y") {
+                move("Y", 1, 15);
+            }
+            // B/B,L to state 16
+            else if (tapeCells[it].symbol == "B") {
+                move("B", -1, 16);
             }
         }
 
         // STATE 16
         else if (state == 16) {
-            // B/0,L ke state 16
-            if (tapeCells[it].symbol == "B") {
-                move("0", -1, 16);
+            // Y/Y,L to state 16
+            if (tapeCells[it].symbol == "Y") {
+                move("Y", -1, 16);
             }
-            // 1/1,L ke state 17
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 17);
+            // 0/Y,R to state 17
+            else if (tapeCells[it].symbol == "0") {
+                move("Y", 1, 17);
+            }
+            // C/C,L to state 11
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 11);
             }
         }
 
         // STATE 17
         else if (state == 17) {
-            // 0/0,L ke state 17
+            // 0/0,R to state 17
             if (tapeCells[it].symbol == "0") {
-                move("0", -1, 17);
+                move("0", 1, 17);
             }
-            // B/B,R ke state 0
+            // Y/Y,R to state 17
+            else if (tapeCells[it].symbol == "Y") {
+                move("Y", 1, 17);
+            }
+            // B/0,L to state 18
             else if (tapeCells[it].symbol == "B") {
-                move("B", 1, 0);
+                move("0", -1, 18, 1);
             }
         }
 
         // STATE 18
         else if (state == 18) {
-            // 0/0,L ke state 18
+            // 0/0,L to state 18
             if (tapeCells[it].symbol == "0") {
                 move("0", -1, 18);
             }
-            // 1/1,R ke state 19
-            else if (tapeCells[it].symbol == "1") {
-                move("1", 1, 19);
-            }
-            // X/X,R ke state 19
-            else if (tapeCells[it].symbol == "X") {
-                move("X", 1, 19);
+            // Y/Y,L to state 16
+            else if (tapeCells[it].symbol == "Y") {
+                move("Y", -1, 16);
             }
         }
 
         // STATE 19
         else if (state == 19) {
-            // 0/X,R ke state 19
-            if (tapeCells[it].symbol == "0") {
-                move("X", 1, 19);
+            // X/0,R to state 19
+            if (tapeCells[it].symbol == "X") {
+                move("0", 1, 19);
             }
-            // X/X,R ke state 20
-            else if (tapeCells[it].symbol == "X") {
-                move("X", 1, 20);
+            // C/C,R to state 20
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 20);
             }
         }
 
         // STATE 20
         else if (state == 20) {
-            // 0/0,R ke state 20
+            // 0/0,R to state 20
             if (tapeCells[it].symbol == "0") {
                 move("0", 1, 20);
             }
-            // B/1,L ke state 21
+            // B/B,R to state 20
             else if (tapeCells[it].symbol == "B") {
-                move("1", -1, 21);
+                move("B", 1, 20);
+            }
+            // C/C,R to state 21
+            else if (tapeCells[it].symbol == "C") {
+                move("C", 1, 21);
             }
         }
 
         // STATE 21
         else if (state == 21) {
-            // 0/0,R ke state 21
+            // 0/0,L to state 21
             if (tapeCells[it].symbol == "0") {
+                move("0", -1, 21);
+            }
+            // Y/0,R to state 21
+            else if (tapeCells[it].symbol == "Y") {
                 move("0", 1, 21);
             }
-            // 1/1,L ke state 21
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 21);
-            }
-            // X/X,L ke state 21
-            else if (tapeCells[it].symbol == "X") {
-                move("X", -1, 21);
-            }
-            // B/0,L ke state 22
-            else if (tapeCells[it].symbol == "B") {
-                move("0", -1, 22);
+            // C/C,L to state 22
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 22);
             }
         }
 
         // STATE 22
         else if (state == 22) {
-            // B/0,L ke state 22
-            if (tapeCells[it].symbol == "B") {
+            // 0/0,L to state 22
+            if (tapeCells[it].symbol == "0") {
                 move("0", -1, 22);
             }
-            // 1/1,L ke state 23
-            else if (tapeCells[it].symbol == "1") {
-                move("1", -1, 23);
+            // B/B,L to state 22
+            else if (tapeCells[it].symbol == "B") {
+                move("B", -1, 22);
+            }
+            // C/C,L to state 8
+            else if (tapeCells[it].symbol == "C") {
+                move("C", -1, 8);
             }
         }
 
         // STATE 23
         else if (state == 23) {
-            // 0/0,L ke state 23
-            if (tapeCells[it].symbol == "0") {
-                move("0", -1, 23);
+            // B/B,L to state 23
+            if (tapeCells[it].symbol == "B") {
+                move("B", -1, 23);
             }
-            // B/B,R ke state 0
-            else if (tapeCells[it].symbol == "B") {
-                move("B", 1, 0);
+            // C/B,L to state 24
+            else if (tapeCells[it].symbol == "C") {
+                move("B", -1, 24);
             }
         }
 
         // STATE 24
         else if (state == 24) {
-            // 0/B,R ke state 24
+            // 0/B,L to state 24
             if (tapeCells[it].symbol == "0") {
+                move("B", -1, 24);
+            }
+            // X/B,L to state 24
+            else if (tapeCells[it].symbol == "X") {
+                move("B", -1, 24);
+            }
+            // B/B,R to state 24
+            else if (tapeCells[it].symbol == "B") {
                 move("B", 1, 24);
             }
-            // 1/B,R ke state 25
-            else if (tapeCells[it].symbol == "1") {
+            // C/B,R to state 25
+            else if (tapeCells[it].symbol == "C") {
                 move("B", 1, 25);
             }
         }
 
-        // STATE 25
+        // STATE 25 (FINAL STATE)
         else if (state == 25) {
-            // X/B,R ke state 25
-            if (tapeCells[it].symbol == "X") {
-                move("B", 1, 25);
-            }
-            // 0/0,R ke state 26
-            else if (tapeCells[it].symbol == "0") {
-                move("0", 1, 26);
-            }
-            // 1/B,R ke state 26
-            else if (tapeCells[it].symbol == "1") {
-                move("B", 1, 26);
-            }
-            // B/0,R ke state 27
-            else if (tapeCells[it].symbol == "B") {
-                move("0", 1, 27);
-            }
-        }
-
-        // STATE 26
-        else if (state == 26) {
-            // 0/0,R ke state 26
-            if (tapeCells[it].symbol == "0") {
-                move("0", 1, 26);
-            }
-            // 1/B,R ke state 27
-            else if (tapeCells[it].symbol == "1") {
-                move("B", 1, 27);
-            }
-        }
-
-        // STATE 27 (FINAL STATE)
-        else if (state == 27) {
             reachedEndState();
         }
     }
